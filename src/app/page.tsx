3f8889d723cfed0ff5e8735581805ab1bdf5d83b
Brijesh Kumar <bk10895@gmail.com>
2024-05-26 12:08:21 +0530

    feat: add Hero component and adjust page layout
    - Implement new Hero component for enhanced design
    - Adjust Home page layout to provide a better user experience
    - Update .prettierrc file configurations for consistent code formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 export default function Home() {
   return (
-    <main className=' relative flex items-center justify-center bg-black-100'>
-      <p className='max-w-lg mx-auto font-semibold text-xl text-white'>
-        Home Page
-      </p>
+    <main className=' relative flex  flex-col overflow-hidden mx-auto sm:px-10 px-5 items-center justify-center bg-black-100'>
+      <div className='max-w-7xl w-full'>
+        <p className='max-w-lg mx-auto font-semibold text-xl text-white'>
+          Home Page
+        </p>
+      </div>
     </main>
   );
 }
