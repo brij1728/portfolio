cd096d6722987727d8faec63d626b73c516cab55
Brijesh Kumar <bk10895@gmail.com>
2024-05-27 22:21:44 +0530

    refactor: modify homepage structure and FloatingNav styling
    - Remove hardcoded navItems from Home component and replace with imported navItems
    - Add 'projects' id to the RecentProjects component for better accessibility and navigation
    - Update FloatingNav component styling to improve visual design and remove redundant login button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,13 @@
 import { Grid, Hero, RecentProjects } from '@/components';
 
-import { FaHome } from 'react-icons/fa';
 import { FloatingNav } from '@/components/ui';
+import { navItems } from '../../data';
 
 export default function Home() {
   return (
     <main className='relative flex flex-col items-center justify-center w-full px-5 mx-auto overflow-hidden bg-black-100 sm:px-10'>
       <div className='w-full max-w-7xl'>
-        <FloatingNav
-          navItems={[{ name: 'Home', link: '/', icon: <FaHome /> }]}
-        />
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         <RecentProjects />
--- src/components/RecentProjects/RecentProjects.tsx
+++ src/components/RecentProjects/RecentProjects.tsx
@@ -5,7 +5,7 @@ import { projects } from '../../../data';
 
 export const RecentProjects = () => {
   return (
-    <div className='py-20'>
+    <div className='py-20' id='projects'>
       <h1 className='heading'>
         A small selection of {''}
         <span className='text-purple'>recent projects</span>
