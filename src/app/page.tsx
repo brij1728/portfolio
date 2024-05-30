256983cf0349c86475067317290a1cd6c392c37a
Brijesh Kumar <bk10895@gmail.com>
2024-05-30 18:14:49 +0530

    style: modify layout and cursor pointer behavior in various components
    - Adjust overflow property in main page layout for better content handling
    - Change margin and padding in Footer component for improved viewing experience
    - Modify cursor behavior on navItem name for enhanced user interaction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ import { navItems } from '../../data';
 
 export default function Home() {
   return (
-    <main className='relative flex flex-col items-center justify-center w-full px-5 mx-auto overflow-hidden bg-black-100 sm:px-10'>
+    <main className='relative flex flex-col items-center justify-center w-full px-5 mx-auto  bg-black-100 sm:px-10 overflow-clip'>
       <div className='w-full max-w-7xl'>
         <FloatingNav navItems={navItems} />
         <Hero />
